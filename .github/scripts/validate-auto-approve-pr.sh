f4d61ec692c14f65bc3812a519b573615a18e041
james-ogunsanwo <james.ogunsanwo@mettle.co.uk>
2023-03-29 15:12:53 +0100

    test

--- .github/scripts/validate-auto-approve-pr.sh
+++ .github/scripts/validate-auto-approve-pr.sh
@@ -2,18 +2,27 @@
 
 set -e
 
-BRANCH=release-test-4554698840
-
 NUMBER_OF_FILE_CHANGES=$(gh pr diff "$BRANCH" --name-only | wc -l)
 if [ $NUMBER_OF_FILE_CHANGES -gt 1 ]; then
-  echo "More than one file change please review manually" && exit 1
+  echo "More than one file change! Please review manually" && exit 1
 fi
 
 echo "Reviewing PR version update"
-gh pr diff $BRANCH  --color never |
+TARGET_REVISION=$(gh pr diff $BRANCH  --color never |
 grep targetRevision |
 tail -n1 |
 cut -c 2- |
-xargs
-#grep -Eq "targetRevision: (0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
-echo "targetRevision updated"
+xargs |
+sed -e "s/^targetRevision: //")
+
+TARGET_REVISION_REGEX="^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
+if [[ $TARGET_REVISION =~ $TARGET_REVISION_REGEX ]];
+then
+    echo "TARGET_REVISION '$TARGET_REVISION' matches TARGET_REVISION_REGEX '$TARGET_REVISION_REGEX'"
+else
+    echo "TARGET_REVISION '$TARGET_REVISION' DOES NOT MATCH TARGET_REVISION_REGEX '$TARGET_REVISION_REGEX'"
+    echo "Validation unsuccessful"
+    exit 1
+fi
+
+echo "Validation successful"
